68119408c9eeec4e67ebf0cc23a2b1d53628331c
danya <d.troyanov@gltd.io>
2025-03-05 10:51:52 +0500

    feat: create project

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
-import { fileURLToPath } from 'url';
-import { dirname, resolve } from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react(), svgr()],
-	resolve: {
-		alias: {
-			'@': resolve(__dirname, './src'),
-		},
-	},
 });
